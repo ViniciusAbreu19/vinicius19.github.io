f4e0a8b497146788e44cdd8f0150414d76ad93b7
ViniciusAbreu19 <136516867+ViniciusAbreu19@users.noreply.github.com>
2025-08-27 20:15:53 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,320 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-fachada-do-vale",
+      "name": "Fachada do Vale",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4706581243435366,
+          "pitch": 0.06407351678891082,
+          "rotation": 0,
+          "target": "4-porto-do-vale"
+        },
+        {
+          "yaw": 2.801807126318873,
+          "pitch": 0.1411775110497384,
+          "rotation": 0,
+          "target": "1-praa-do-vale"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-praa-do-vale",
+      "name": "Praça do Vale",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.19696035399149991,
+          "pitch": -0.06112453444037946,
+          "rotation": 0,
+          "target": "0-fachada-do-vale"
+        },
+        {
+          "yaw": 3.0462277983250905,
+          "pitch": -0.01816834525095956,
+          "rotation": 0,
+          "target": "2-vista-do-vale"
+        },
+        {
+          "yaw": 1.262045687643555,
+          "pitch": 0.010564647509474057,
+          "rotation": 0,
+          "target": "3-escudo-de-so-joo"
+        },
+        {
+          "yaw": 2.3417800848994617,
+          "pitch": 0.1403431220187077,
+          "rotation": 0,
+          "target": "5-espao-cerimonial"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.04321112413952832,
+          "pitch": -0.41228329180873047,
+          "title": "Morte e Redenção",
+          "text": "<strong data-start=\"383\" data-end=\"401\">Virgem Maria</strong> amparando o corpo de Cristo (Pietà). Esses conjuntos reforçam a dimensão religiosa do monumento, evocando sacrifício, dor e esperança na vida eterna. Além da mensagem cristã, a grandiosidade das figuras foi pensada para exaltar solenidade e transmitir a ideia de que o sacrifício dos mortos da Guerra Civil se unia ao sacrifício de Cristo."
+        },
+        {
+          "yaw": 0.034869531590615566,
+          "pitch": -0.6917813068941143,
+          "title": "Cruz Monumental",
+          "text": "Com 152 metros de altura, a cruz do Vale dos Caídos é uma das maiores do mundo e pode ser avistada a dezenas de quilômetros de distância. Mais do que um símbolo religioso, ela foi concebida como um marco de poder e redenção, associando a Guerra Civil à ideia de “cruzada”. Sua dimensão colossal coroa o conjunto arquitetônico, tornando-se o elemento mais icônico do local."
+        }
+      ]
+    },
+    {
+      "id": "2-vista-do-vale",
+      "name": "Vista do Vale",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2684450535082732,
+          "pitch": -0.07413579879828092,
+          "rotation": 0,
+          "target": "5-espao-cerimonial"
+        },
+        {
+          "yaw": 0.6931028380401525,
+          "pitch": -0.019752561511303668,
+          "rotation": 0,
+          "target": "3-escudo-de-so-joo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-escudo-de-so-joo",
+      "name": "Escudo de São João",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1539995442442201,
+          "pitch": 0.06298637190708156,
+          "rotation": 0,
+          "target": "1-praa-do-vale"
+        },
+        {
+          "yaw": -2.4178543923693105,
+          "pitch": 0.02481721327629316,
+          "rotation": 0,
+          "target": "2-vista-do-vale"
+        },
+        {
+          "yaw": -2.007076801382798,
+          "pitch": 0.11388933906395238,
+          "rotation": 0,
+          "target": "5-espao-cerimonial"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6856832743455961,
+          "pitch": -0.22306307175882267,
+          "title": "Escudo de São João",
+          "text": "O escudo de São João, presente em diferentes áreas do monumento, traz a águia imperial associada ao evangelista João. Esse símbolo foi recuperado por Franco para remeter ao esplendor da monarquia de Carlos V e transmitir uma imagem de poder, unidade e continuidade histórica. No Vale, reforça a simbologia de triunfo do regime franquista."
+        }
+      ]
+    },
+    {
+      "id": "4-porto-do-vale",
+      "name": "Portão do Vale",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6067347637928684,
+          "pitch": 0.1914792492205173,
+          "rotation": 0,
+          "target": "0-fachada-do-vale"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-espao-cerimonial",
+      "name": "Espaço Cerimonial",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6854268967420065,
+          "pitch": -0.10785285285713897,
+          "rotation": 0,
+          "target": "3-escudo-de-so-joo"
+        },
+        {
+          "yaw": -2.5170630593351078,
+          "pitch": 0.004456029335585754,
+          "rotation": 0,
+          "target": "2-vista-do-vale"
+        },
+        {
+          "yaw": -0.251644011862977,
+          "pitch": -0.09453147132288109,
+          "rotation": 0,
+          "target": "1-praa-do-vale"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6106844969329295,
+          "pitch": 0.26243338217711987,
+          "title": "Espaço Cerimonial",
+          "text": "A praça externa é o amplo espaço que antecede a basílica do Vale dos Caídos. Com uma escadaria monumental e proporções grandiosas, foi projetada para receber cerimônias públicas, missas campais e concentrações políticas durante a ditadura. Seu caráter aberto e imponente reforça a monumentalidade do complexo e prepara o visitante para a entrada na basílica."
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
